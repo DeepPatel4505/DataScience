3b3332a6e51d54d0dbabf933a2c81f2cea8c30d0
Deep Patel <deeppatel5501@gmail.com>
2025-09-14 15:05:15 +0530

    Add unit tests for SimpleGD model and fix initialization typo
    - Created a new test file SimpleGD.test.py to evaluate the performance of the SimpleGD model against sklearn's SGDRegressor.
    - Implemented a regression dataset using sklearn's make_regression and visualized it with matplotlib.
    - Added R² score calculations for both SimpleGD and SGDRegressor to compare their performance.
    - Fixed a typo in the SimpleGD class constructor from __inti__ to __init__.
    - Adjusted loss calculations in the fit method to include normalization by the number of training samples.

--- models/SimpleGD.py
+++ models/SimpleGD.py
@@ -1,20 +1,21 @@
 import numpy as np
 
 class SimpleGD:
-    def __inti__(self,learning_rate = 0.1,epochs = 100):
+    def __init__(self,learning_rate = 0.1,epochs = 100):
         self.m = 0
         self.b = 0
         self.learning_rate = learning_rate
         self.epochs = epochs
     
     def fit(self,x_train,y_train):
+        n = len(x_train)
         for i in range(self.epochs):
-            loss_m = -2 * np.sum(y_train - self.m*x_train.ravel() - self.b)
-            loss_b = -2 * np.sum((y_train - self.m*x_train.ravel() - self.b) * x_train.ravel())
+            loss_m = (-2/n) * np.sum((y_train - self.m*x_train.ravel() - self.b) * x_train.ravel())
+            loss_b = (-2/n)* np.sum(y_train - self.m*x_train.ravel() - self.b)
             
             self.m = self.m - (self.learning_rate * loss_m)
             self.b = self.b - (self.learning_rate * loss_b)
-        
+        print(self.m,self.b)
     
     def predict(self,x_test):
         return self.m * x_test + self.b 
